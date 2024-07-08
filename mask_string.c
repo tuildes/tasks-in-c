f14f25b654f3cf0f3266d1184968ad82a0dc306f
tuildes <frehsegustavo@gmail.com>
2024-07-08 10:22:08 -0300

    feat: algoritmo de mascara

---
+++ mask_string.c
@@ -0,0 +1,30 @@
+#define MASK_SIZE 4;
+
+// Array size of masked == string
+char *maskify (char *masked, const char *string) {
+  unsigned int lenght = 0;
+  
+  // Pega o tamanho da sua palavra
+  for (unsigned int i = 0; string[i] != '\0'; i++)
+    lenght++;
+  
+  // Finaliza a palavra
+  masked[lenght] = '\0';
+  if (lenght <= MASK_SIZE) {
+    
+    for (unsigned int i = 0; i < lenght; i++)
+      masked[i] = string[i];
+    
+  } else {
+    
+    // Coloca a mascara
+    for (unsigned int i = 0; i < (lenght - MASK_SIZE); i++)
+      masked[i] = '#';
+    
+    for (unsigned int i = 1; i <= MASK_SIZE; i++)
+      masked[lenght-i] = string[lenght-i];
+    
+  }
+  
+	return masked; // return it
+}
