2f5706ce64760c0f5b13e51129eb4c37fcc85997
tuildes <frehsegustavo@gmail.com>
2024-07-21 11:39:20 -0300

    feat: somador de dois numeros

---
+++ beecrowd/sum_two_numbers/program.c
@@ -0,0 +1,34 @@
+// Includes de todo o projeto
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <time.h>
+
+// Defines de todo o projet
+#define BEECROWD_EXERCISE 1024
+#define SUM(a, b) (a + b)
+
+// Global variables
+
+// Functions
+/**
+ *  Function name
+ *  Function description
+ *
+ *  @param { type } name - use
+ *  @return { type } - use
+ * 
+ **/
+
+int main (/* int argc, char *argv[] */) {
+
+    printf("Exercício da beecrowd: %d\n\n", BEECROWD_EXERCISE);
+
+    int a, b;
+
+    scanf("%d%d", &a, &b);
+    printf("X = %d\n", SUM(a,b));
+
+    return 0;
+}
