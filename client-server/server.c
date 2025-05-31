e6406759e56949dcd37fcaf99a4f1d3e919df5e8
tuildes <frehsegustavo@gmail.com>
2025-05-31 17:16:48 -0300

    fix: Remove unnecessary files and update server cod

--- client-server/server.c
+++ client-server/server.c
@@ -1,6 +1,5 @@
-/* Um servidor TCP/IP Iterativo
-   Elias P. Duarte Jr. 
-   Ãšltima AtualizaÃ§Ã£o: 17/08/22 */
+/* 	Um servidor TCP/IP Iterativo
+	Baseado no codigo de Elias P. Duarte Jr. */
 
 #include <stdio.h>
 #include <sys/socket.h>
@@ -14,56 +13,61 @@
 #define TAMFILA      5
 #define MAXHOSTNAME 30
 
+void cleanBuffer(char buffer[]) {
+	for(int i = 0; i < BUFSIZ; i++) 
+		buffer[i] = '\0';
+}
+
 int main (int argc, char *argv[]) {
-	int s, t;
-	int i;
-        char buf [BUFSIZ + 1];
+	int socketList, socketAccept;
+	unsigned int i;
+        char buffer [BUFSIZ + 1];
 	struct sockaddr_in sa, isa;
 	struct hostent *hp;
 	char localhost [MAXHOSTNAME];
 
-        if (argc != 2) {
-           puts("Uso correto: servidor <porta>");
-           exit(1);
-        }
+	if (argc != 2) {
+		puts("Uso correto: servidor <porta>");
+		exit(1);
+	}
 
 	gethostname (localhost, MAXHOSTNAME);
 
 	if ((hp = gethostbyname( localhost)) == NULL){
-		puts ("Nao consegui meu proprio IP");
-		exit (1);
+		puts("Nao consegui meu proprio IP");
+		exit(1);
 	}	
 	
 	sa.sin_port = htons(atoi(argv[1]));
 
-	bcopy ((char *) hp->h_addr, (char *) &sa.sin_addr, hp->h_length);
+	bcopy((char *) hp->h_addr, (char *) &sa.sin_addr, hp->h_length);
 
-	sa.sin_family = hp->h_addrtype;		
+	sa.sin_family = hp->h_addrtype;
 
-
-	if ((s = socket(hp->h_addrtype,SOCK_STREAM,0)) < 0){
-           puts ( "Nao consegui abrir o socket" );
-		exit (1);
+	if ((socketList = socket(hp->h_addrtype,SOCK_STREAM,0)) < 0){
+        puts("Nao consegui abrir o socket");
+		exit(1);
 	}	
 
-	if (bind(s, (struct sockaddr *) &sa, sizeof(sa)) < 0){
-		puts ( "Nao consegui fazer o bind" );
-		exit (1);
+	if (bind(socketList, (struct sockaddr *) &sa, sizeof(sa)) < 0){
+		puts("Nao consegui fazer o bind");
+		exit(1);
 	}		
  
-	listen (s, TAMFILA);
+	listen (socketList, TAMFILA);
 	
 	while (1){
 		i = sizeof(sa);
-		if ((t=accept(s, (struct sockaddr *) &isa,&i))<0){
-                   puts ( "Nao consegui estabelecer conexao" );
-                   exit (1);
+		if ((socketAccept = accept(socketList, (struct sockaddr *) &isa, &i)) < 0){
+			puts("Nao consegui estabelecer conexao");
+			exit(1);
 		}	
 
-       read(t, buf, BUFSIZ);
-       printf("Sou o servidor, recebi a mensagem----> %s\n", buf);
-       write(t,buf, BUFSIZ);
-       close(t);
+		cleanBuffer(buffer);
+		read(socketAccept, buffer, BUFSIZ);
+		printf("Sou o servidor, recebi a mensagem: %s\n", buffer);
+		write(socketAccept, buffer, BUFSIZ);
+		close(socketAccept);
    }
    exit(0);
 }
