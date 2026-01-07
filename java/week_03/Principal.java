36e1a0807b59bf9055736d21b74dc4b4f42ec470
tuildes <frehsegustavo@gmail.com>
2026-01-07 12:03:07 -0300

    ajustes nas pastas

--- java/week_03/Principal.java
+++
@@ -1,6 +0,0 @@
-public class Principal {
-    public static void main(String[] args) {
-        Livro livro = new Livro("Cinco Linguagens do Amor", 2013, new Autor("Gary Chapman", "Ingles"));
-        livro.exibir();
-    }
-}
