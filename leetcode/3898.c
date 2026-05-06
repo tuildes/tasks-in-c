e475ae1ca4423608162d195376532e94c3526bdb
tuildes <frehsegustavo@gmail.com>
2026-05-06 13:48:44 -0300

    changed the beecrowd to root

---
+++ leetcode/3898.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+
+int* findDegrees(int** matrix, int matrixSize, int* matrixColSize, int* returnSize) {
+
+    int *res = malloc(sizeof(int) * matrixSize);
+    if (res == NULL) return NULL;
+
+    for (int i = 0; i < matrixSize; i++) {
+        res[i] = 0;
+
+        for (int j = 0; j < matrixColSize[i]; j++) {
+            if (matrix[i][j]) res[i]++;
+        }
+    }
+
+    *returnSize = matrixSize;
+    return res;
+}
