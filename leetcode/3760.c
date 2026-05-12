86b515ee6d093404214970bc6b66d52fa480c207
tuildes <frehsegustavo@gmail.com>
2026-05-12 14:07:19 -0300

    feat: leetcode 3760

---
+++ leetcode/3760.c
@@ -0,0 +1,15 @@
+int maxDistinct(char* s) {
+    int total[26] = { 0 };
+    int res = 0;
+    int position;
+
+    for(int i = 0; s[i] != '\0'; i++) {
+        position = (s[i] - 96);
+        total[position] += 1;
+
+        if (total[position] == 1)
+            res += 1;
+    }
+
+    return res;
+}
